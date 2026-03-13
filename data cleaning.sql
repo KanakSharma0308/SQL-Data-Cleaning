fbc1045929957cef08ced9658d0a803dcf58d5b6
KanakSharma0308 <kanaksh7691@gmail.com>
2026-03-13 13:09:56 -0700

    Add files via upload

---
+++ data cleaning.sql
@@ -0,0 +1,134 @@
+select * from layoffs.layoffs;
+-- 1 REMOVE DUPLICATE
+-- 2. STANDARDIZE DATA
+-- 3. NULL VALUES 
+-- 4. REMOVE ANY COLUMNS
+
+CREATE TABLE layoff_staging
+like layoffs.layoffs;
+insert layoff_staging
+select * from layoffs.layoffs;
+select * from layoff_staging ;
+
+select * ,
+row_number () over(partition by company,location, industry,total_laid_off,percentage_laid_off,'date',stage,country,funds_raised_millions)  as row_num
+from layoff_staging ;
+
+with duplicate_cte as (
+select * ,
+row_number () over(partition by company, location, industry,total_laid_off,percentage_laid_off,'date', stage,country,funds_raised_millions )  as row_num
+from layoff_staging)
+select * from duplicate_cte 
+where row_num > 1 ;
+
+select * from layoff_staging 
+where company='casper';
+
+  CREATE TABLE `layoff_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+   `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select * from layoff_staging2 ;
+
+insert into layoff_staging2 
+select * ,
+row_number () over(partition by company, location, industry,total_laid_off,percentage_laid_off,'date', stage,country,funds_raised_millions )  as row_num
+from layoff_staging ;
+
+select * from layoff_staging2 where row_num > 1;
+set SQL_SAFE_UPDATES = 0;
+
+delete 
+from layoff_staging2 
+where row_num > 1;
+
+
+select * from layoff_staging2 ;
+
+
+-- standadizing data
+select company,trim(company) from layoff_staging2;
+
+update layoff_staging2
+set company=trim(company);
+
+select distinct (industry) from layoff_staging2 order by 1;
+
+select * from layoff_staging2 where industry like 'crypto%';
+
+
+update layoff_staging2
+set  industry = 'crypto'
+where industry like 'crypto%';
+
+select * from layoff_staging2 where industry like 'crypto%';
+
+select distinct (country) from layoff_staging2 order by 1;
+
+select * from layoff_staging2 where country like 'United%';
+
+update layoff_staging2
+set  country = 'United States'
+where country like 'United%';
+
+select distinct (country) from layoff_staging2 order by 1;
+
+
+select * from layoff_staging2;
+
+select `date` ,str_to_date(`date`,'%m/%d/%Y') from  layoff_staging2;
+
+update layoff_staging2
+set  `date`=str_to_date(`date`,'%m/%d/%Y');
+
+
+alter table layoff_staging2 modify column `date` date;
+
+-- removing null and blanks 
+
+select * from layoff_staging2 where total_laid_off is null and percentage_laid_off is null;
+
+select * from layoff_staging2 where industry is null or industry="";
+select * from layoff_staging2 t1 join layoff_staging2 t2
+on t1.company=t2.company and t1.location=t2.location 
+where (t1.industry is null or t1.industry='')
+ and t2.industry is not null;
+ 
+ update layoff_staging2 t1
+ set t1.industry=null
+ where industry='';
+ 
+ 
+ update layoff_staging2 t1 join layoff_staging2 t2
+on t1.company=t2.company and t1.location=t2.location 
+set t1.industry=t2.industry
+where (t1.industry is null)
+ and t2.industry is not null;
+
+select * from layoff_staging2 where industry="";
+
+-- delete column 
+select * from layoff_staging2 where total_laid_off is null and percentage_laid_off is null;
+delete from layoff_staging2 where total_laid_off is null and percentage_laid_off is null;
+
+ALTER TABLE layoff_staging2
+drop column row_num;
+
+
+select * from layoff_staging2;
+
+
+
+
+
+
+
